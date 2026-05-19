07e18c1c280c6a31222e511b0a3e849d422c2b33
flavio-carddoso <phavio2010@gmail.com>
2026-05-19 16:52:39 -0300

    Atualizando o arquivo ConnectionFactory

--- caleum.java.web.fj21/src/br/com/caleum/f21/ConnectionFactory.java
+++ caleum.java.web.fj21/src/br/com/caleum/f21/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	public Connection getConnection(){				
 		try {	
-			return DriverManager.getConnection("jdbc:mysql://localhost/dbagenda", "root", "Fl@817362");
+			return DriverManager.getConnection("jdbc:mysql://localhost/dbagenda", "root", "senhadobanco");
 		} catch (SQLException e) {
 
 			throw new RuntimeException(e);
